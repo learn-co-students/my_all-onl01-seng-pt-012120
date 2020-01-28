c91348b71dd297c1b38321c73452fd414f78b875
James Rogers <jarogers095@gmail.com>
2020-01-28 06:10:10 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  if block_given?
+    final_result = true
+    i = 0
+    while i < collection.size do
+      final_result = yield(collection[i])
+      i += 1
+    end
+    return final_result
+  else
+    return nil
+  end
 end
